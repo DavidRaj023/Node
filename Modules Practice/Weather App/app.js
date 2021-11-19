4f084c4034fa0d9e719b6ceeda9a47d95e66ec19
DavidRaj023 <benjamindavid023@gmail.com>
2021-11-19 14:44:10 +0530

    Weather app update

--- Modules Practice/Weather App/app.js
+++ Modules Practice/Weather App/app.js
@@ -1,7 +1,12 @@
 const weather = require('./utill/weather')
 
-const url = 'http://api.weatherstack.com/current?access_key=ad9cc9ccee682e5f4b011a619badab5a&query=Trichy'
+const place = process.argv[2]
 
-weather(url, (data) =>{
-    console.log(data)
-})
+if (place){
+    const url = 'http://api.weatherstack.com/current?access_key=ad9cc9ccee682e5f4b011a619badab5a&query='+place
+    weather(url, (data) =>{
+        console.log(data)
+    })    
+} else{
+    console.log('Please Enter the City Name')
+}
